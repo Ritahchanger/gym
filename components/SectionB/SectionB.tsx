290107211c6320099645af45014c807db30e8f74
Dennis Peter <peterdennis573@gmail.com>
2024-10-10 20:19:52 +0300

    Updates on the universal buttons

--- components/SectionB/SectionB.tsx
+++ components/SectionB/SectionB.tsx
@@ -1,5 +1,5 @@
 import NavigateArrow from "../NavigateArrow/NavigateArrow";
-
+import { IoMdAddCircle } from "react-icons/io";
 const SectionB = () => {
   return (
     <div className="mt-5">
@@ -29,15 +29,49 @@ const SectionB = () => {
             <p className="text-5xl font-semibold text-center">
               Flexible membership plans to suit your pocket
             </p>
-            <div className="grid grid-cols-1 md:grid-cols-3 mt-8 gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 mt-8 gap-5">
               <div className="card bg-slate-700 p-3 rounded-lg">
                 <p className="text-3xl text-center">Advanced</p>
+
+                <button className="universal-btn">
+                  Join{" "}
+                  <span>
+                    <IoMdAddCircle />
+                  </span>
+                </button>
+                <p>
+                  Join our Advanced Membership to elevate your fitness journey!
+                  Designed for those who are serious about achieving their
+                  goals, this plan offers:
+                </p>
               </div>
               <div className="card bg-slate-700 p-3 rounded-lg">
                 <p className="text-3xl text-center">Basic</p>
+                <button className="universal-btn">
+                  Join{" "}
+                  <span>
+                    <IoMdAddCircle />
+                  </span>
+                </button>
+                <p>
+                  Join our Basic Membership to elevate your fitness journey!
+                  Designed for those who are serious about achieving their
+                  goals, this plan offers:
+                </p>
               </div>
               <div className="card bg-slate-700 p-3 rounded-lg">
                 <p className="text-3xl text-center">Premium</p>
+                <button className="universal-btn">
+                  Join{" "}
+                  <span>
+                    <IoMdAddCircle />
+                  </span>
+                </button>
+                <p>
+                  Join our Premium Membership to elevate your fitness journey!
+                  Designed for those who are serious about achieving their
+                  goals, this plan offers:
+                </p>
               </div>
             </div>
           </div>
