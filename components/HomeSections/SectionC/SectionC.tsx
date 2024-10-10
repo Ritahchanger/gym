7ac99eabe8a87638eff3fb66fb09915430ff2539
Dennis Peter <peterdennis573@gmail.com>
2024-10-10 21:16:38 +0300

    Updates on the homepage

--- components/HomeSections/SectionC/SectionC.tsx
+++ components/HomeSections/SectionC/SectionC.tsx
@@ -15,21 +15,35 @@ const SectionC = () => {
             <button>Success</button>
           </div>
           <div className="grid sm:grid-cols-1 md:grid-cols-2 g">
-            <div className="p-3 bg-slate-700 flex items-center justify-center">
+            <div className="p-3 bg-slate-700 flex-row items-center justify-center relative">
               <img
                 src="https://images.pexels.com/photos/6551174/pexels-photo-6551174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 alt=""
+                className="h-full"
               />
+              <div className="absolute bottom-6 left-[2rem]">
+                <p className="text-1xl">Sasha Angelica</p>
+                <button className="bg-orange-600 sharp px-3 border">
+                  Read more
+                </button>
+              </div>
             </div>
-            <div className="p-3 bg-slate-700 flex items-center justify-center">
+            <div className="p-3 bg-slate-700 flex-row items-center justify-center relative">
               <img
                 src="https://images.pexels.com/photos/6551174/pexels-photo-6551174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 alt=""
+                className="h-full"
               />
+              <div className="absolute bottom-6 left-[2rem]">
+                <p className="text-1xl">Sasha Angelica</p>
+                <button className="bg-orange-600 sharp px-3 border">
+                  Read more
+                </button>
+              </div>
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-4 mt-[3rem] gap-4">
+        <div className="grid grid-cols-4 mt-[3rem] gap-4 ">
           <div className="bg-slate-800 py-2 px-2">
             <p className="text-3xl font-semibold">6,3453</p>
             <p>
