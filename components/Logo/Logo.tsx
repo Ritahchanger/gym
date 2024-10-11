abb4610bf97c8f5ff9ec55ce013b0504b81619fb
Dennis Peter <peterdennis573@gmail.com>
2024-10-11 11:21:37 +0300

    updates on the navbar

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <div className="text-[var(--pumpkinOrange)] text-4xl font-semibold">
-      <Link href="/">GyMo</Link>
+      <Link href="/">Mo..</Link>
     </div>
   );
 };
